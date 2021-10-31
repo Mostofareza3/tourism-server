acac3bb8584bb978fb4aac23725e29186afcd174
Mostofareza3 <86896410+Mostofareza3@users.noreply.github.com>
2021-10-31 15:33:36 +0600

    second commit

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 console.log(uri)
 
 client.connect(err => {
-    // console.log('connected to database')
     const availablePackages = client.db("TravelX").collection("packages");
     const booksCollection = client.db("TravelX").collection("books");
 
+    //add packages
     app.post('/addPackages', async (req, res) => {
         const package = req.body;
         const result =await availablePackages.insertOne(package)
@@ -35,13 +35,6 @@ client.connect(err => {
 
         });
 
-        //delete product
-        app.delete('/deleteProducts/:id', (req, res) => {
-            const id = req.params.id;
-            const result = productsCollection.deleteOne({ _id: ObjectId(id) })
-            res.send(result)
-        })
-
         // get single product
         app.get('/placeBooking/:id', async (req, res) => {
             const id = req.params.id;
@@ -49,50 +42,31 @@ client.connect(err => {
             res.send(result)
         });
 
-        //update products
-        app.put('/update/:id', (req, res) => {
-            const id = req.params.id;
-            const updatedInfo = req.body;
-            const filter = { _id: ObjectId(id) };
-            console.log(filter)
-
-            productsCollection.updateOne(filter, {
-                $set: {
-                    name: updatedInfo.name,
-                    price: updatedInfo.price
-                }
-            })
-                .then(result => res.send(result))
-
-        });
         // addedItems
-        app.post('/addedItem/:id', async(req,res)=>{
-            const id = req.params.id;
-            const result = await availablePackages.findOne({_id: ObjectId(id)});
-            booksCollection.insertOne(result);
-            res.send(result)
+        app.post('/addedItem', async(req,res)=>{
+           
+           const result = await booksCollection.insertOne(req.body);
+            if(result){
+                res.send(result)
+            }
+          
         })
 
         //get added item
-        app.get('/myOrders', (req,res)=>{
-            const result = booksCollection.find({}).toArray();
-            console.log(result)
-        })
-
-        //add order old
-        app.post('/addedOrder', (req, res) => {
-            ordersCollection.insertOne(req.body)
-                .then(result => res.send(result))
-        });
-
-        // my cart old
-        app.get('/myCart/:email', async (req, res) => {
-
-
-            const result = await ordersCollection.find({ email: req.params.email }).toArray();
+        app.get('/myOrders',async (req,res)=>{
+            const result =await booksCollection.find({}).toArray();
             res.send(result)
-
+        });
+        
+        //delete order
+        app.delete('/deleteOrder/:email',async (req, res) => {
+            const result = await booksCollection.deleteOne({
+                email: req.params.email
+              });
+              res.send(result)
         })
+
+       
     });
 
 
